496c29b19c2b696b7dbc75867a4539e3403051ac
Felipe <felipe@webgoal.com.br>
2020-05-06 22:38:20 -0300

    estrutura basica de persistencia do redux

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Home from './src/screens/Home';
 import { Provider } from 'react-redux';
-import store from './src/store';
+import { store, persistor }  from './src/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 export default function App() {
   return (
     <Provider store={store}> 
-      <Home/>
+      <PersistGate persistor={persistor}>
+        <Home/>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,16 @@
 import { createStore } from 'redux';
+import { persistStore, persistReducer } from "redux-persist";
 import combinedReducers from '../reducers/combinedReducers';
+import storage from 'redux-persist/lib/storage';
 
-const store = createStore(combinedReducers);
+const persistConfig = {
+    key: 'root',
+    storage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, combinedReducers)
+
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
+
+export { store, persistor };
